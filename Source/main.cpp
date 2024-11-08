a352741c1699d2b91a8392c5c40bf64dfa6196dd
Josiah Aviso <150400184+WaywardTaco@users.noreply.github.com>
2024-11-08 08:10:00 +0800

    first commit

---
+++ Source/main.cpp
@@ -0,0 +1,13 @@
+
+#include <iostream>
+
+int main(){
+    if(__cplusplus == 202002L)
+        std::cout << "C++20" << std::endl;
+    else if(__cplusplus == 201703L)
+        std::cout << "C++17" << std::endl;
+    else if(__cplusplus == 201402L)
+        std::cout << "C++14" << std::endl;
+
+    return 0;
+}
