39fb57828f28cd907c41ba2c704fc0f9aeadde83
Josiah Aviso <150400184+WaywardTaco@users.noreply.github.com>
2024-11-28 10:54:41 +0800

    Call the MonitorText Manager's awaitUserInput with a callbackfunction that has string input, this will await user input iuntil enter is pressed then call that callback

--- Assets/Scripts/MonitorTextManager.cs
+++ Assets/Scripts/MonitorTextManager.cs
@@ -4,13 +4,19 @@ using System.ComponentModel;
 using Mysqlx.Crud;
 using TMPro;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class MonitorTextManager : MonoBehaviour
 {
     public static MonitorTextManager Instance { get; private set;}
     [SerializeField] private TMP_Text montiorTextField;
-    [SerializeField] private string monitorText;
+    [SerializeField] private string savedMonitorText;
+    [SerializeField] private string lastMonitorText;
+    [SerializeField] private string inputText;
+    [SerializeField] private bool isTakingInput = false;
+    [SerializeField] private bool isMirroringInput = false;
     [SerializeField] private bool debugUpdateText;
+    [SerializeField] private UnityEvent<string> currentInputListeners;
 
     // Start is called before the first frame update
     void Start()
@@ -27,27 +33,123 @@ public class MonitorTextManager : MonoBehaviour
     public void Update(){
         if(debugUpdateText){
             debugUpdateText = false;
-            this.SetMonitorText(this.monitorText);
+            this.SetMonitorText(this.savedMonitorText);
         }
+        if(isTakingInput)
+            this.HandleInputs();
+        if(isMirroringInput)
+            this.AppendText(this.inputText, false);
     }
 
-    public void SetMonitorText(string text){
-        string newText = formatText(text);
+    public void SetMonitorText(string text, bool saveText = true){
+        string newText = FormatText(text);
 
-        this.monitorText = newText;
+        this.lastMonitorText = this.savedMonitorText;
+        if(saveText)
+            this.savedMonitorText = newText;
         this.montiorTextField.text = newText;
     }
-    public void AppendText(string text){
-        string newText = this.monitorText + text;
+    public void AppendText(string text, bool saveText = true){
+        string newText = this.savedMonitorText + text;
 
-        this.monitorText = newText;
+        this.lastMonitorText = this.savedMonitorText;
+        if(saveText)
+            this.savedMonitorText = newText;
         this.montiorTextField.text = newText;
     }
 
-    private string formatText(string text){
+    public void RevertToSavedText(bool savePrev = true){
+        string savedText = this.savedMonitorText;
+        if(savePrev)
+            this.savedMonitorText = this.montiorTextField.text;
+        this.SetMonitorText(savedText, false);
+    }
+
+    public void RevertToLastText(bool saveText = true){
+        this.SetMonitorText(this.lastMonitorText, saveText);
+    }
+
+    public void AwaitUserInput(UnityAction<string> callbackFunction){
+        this.currentInputListeners.AddListener(callbackFunction);
+        this.isTakingInput = true;
+        this.isMirroringInput = true;
+    }
+
+    private void SubmitInput(){
+        this.AppendText(this.inputText);
+        this.isTakingInput = false;
+        this.isMirroringInput = false;
+
+        this.currentInputListeners.Invoke(this.inputText);
+    }
+
+    private string FormatText(string text){
         return text;
     }
-    public string getText(){
-        return this.monitorText;
+    public string GetText(){
+        return this.savedMonitorText;
+    }
+
+    private void HandleInputs(){
+        if(Input.GetKeyDown(KeyCode.Q))
+            this.inputText += "Q";
+        if(Input.GetKeyDown(KeyCode.W))
+            this.inputText += "W";
+        if(Input.GetKeyDown(KeyCode.E))
+            this.inputText += "E";
+        if(Input.GetKeyDown(KeyCode.R))
+            this.inputText += "R";
+        if(Input.GetKeyDown(KeyCode.T))
+            this.inputText += "T";
+        if(Input.GetKeyDown(KeyCode.Y))
+            this.inputText += "Y";
+        if(Input.GetKeyDown(KeyCode.U))
+            this.inputText += "U";
+        if(Input.GetKeyDown(KeyCode.I))
+            this.inputText += "I";
+        if(Input.GetKeyDown(KeyCode.O))
+            this.inputText += "O";
+        if(Input.GetKeyDown(KeyCode.P))
+            this.inputText += "P";
+        if(Input.GetKeyDown(KeyCode.A))
+            this.inputText += "A";
+        if(Input.GetKeyDown(KeyCode.S))
+            this.inputText += "S";
+        if(Input.GetKeyDown(KeyCode.D))
+            this.inputText += "D";
+        if(Input.GetKeyDown(KeyCode.F))
+            this.inputText += "F";
+        if(Input.GetKeyDown(KeyCode.G))
+            this.inputText += "G";
+        if(Input.GetKeyDown(KeyCode.H))
+            this.inputText += "H";
+        if(Input.GetKeyDown(KeyCode.J))
+            this.inputText += "J";
+        if(Input.GetKeyDown(KeyCode.K))
+            this.inputText += "K";
+        if(Input.GetKeyDown(KeyCode.L))
+            this.inputText += "L";
+        if(Input.GetKeyDown(KeyCode.Z))
+            this.inputText += "Z";
+        if(Input.GetKeyDown(KeyCode.X))
+            this.inputText += "X";
+        if(Input.GetKeyDown(KeyCode.C))
+            this.inputText += "C";
+        if(Input.GetKeyDown(KeyCode.V))
+            this.inputText += "V";
+        if(Input.GetKeyDown(KeyCode.B))
+            this.inputText += "B";
+        if(Input.GetKeyDown(KeyCode.N))
+            this.inputText += "N";
+        if(Input.GetKeyDown(KeyCode.M))
+            this.inputText += "M";
+
+        if(Input.GetKeyDown(KeyCode.Space))
+            this.inputText += " ";
+        if(Input.GetKeyDown(KeyCode.Return))
+            this.SubmitInput();
+            // this.inputText += "\n";
+        if(Input.GetKeyDown(KeyCode.Backspace))
+            this.inputText = this.inputText.Substring(0, this.inputText.Length - 1);
     }
 }
